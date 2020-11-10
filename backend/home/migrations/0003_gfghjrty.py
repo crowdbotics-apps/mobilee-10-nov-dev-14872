d2b3b1c3d66872b168ea154cabda02140132f716
Crowdbotics <team@crowdbotics.com>
2020-11-10 13:02:01 +0000

    Update models

--- backend/home/migrations/0003_gfghjrty.py
+++ backend/home/migrations/0003_gfghjrty.py
@@ -7,15 +7,30 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Gfghjrty',
+            name="Gfghjrty",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('fgwrs', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='gfghjrty_fgwrs', to='home.HomePage')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "fgwrs",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="gfghjrty_fgwrs",
+                        to="home.HomePage",
+                    ),
+                ),
             ],
         ),
     ]
