017286ba9773d57e7b6f7e233e87f5e77dbf393e
Crowdbotics <team@crowdbotics.com>
2020-11-10 13:07:19 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -11,10 +11,6 @@ class User(AbstractUser):
         blank=True,
         max_length=255,
     )
-    dagfreagfc = models.BigIntegerField(
-        null=True,
-        blank=True,
-    )
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
